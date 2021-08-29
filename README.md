24030127ec453bde9ce7abcd87d6c4c0196c4b11
吃柠檬的企鹅 <decezz@qq.com>
2021-08-29 11:57:39 +0800

    swagger

---
+++ README.md
@@ -0,0 +1,7 @@
+## swagger
+
+#### 前端展示资源
+* 前后端分离，该项目只有前端资源
+* 需要配合 `omega` 服务端使用
+* 后端服务地址 `git clone https://gitee.com/decezz/omega.git`
+
---
+++ src/config.js
@@ -0,0 +1,6 @@
+window.AppApi = {
+    aes: '/omega/index.php?s=api/trans',
+    cipherMethods: '/omega/index.php?s=api/trans',
+    forward: '/omega/index.php?s=api/trans',
+    openssl: '/omega/index.php?s=api/trans'
+};
