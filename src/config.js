476ad898010124b1885c4ebaaf076aa01c8beb6d
吃柠檬的企鹅 <decezz@qq.com>
2021-10-04 09:09:35 +0800

    '下载页面'

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 window.AppApi = {
-    aes: '/omega/index.php?s=api/trans',
-    cipherMethods: '/omega/index.php?s=api/trans',
-    forward: '/omega/index.php?s=api/trans',
-    openssl: '/omega/index.php?s=api/trans'
+    aes: '/omega/index.php?s=api/Trans',
+    cipherMethods: '/omega/index.php?s=api/Trans',
+    forward: '/omega/index.php?s=api/Trans',
+    openssl: '/omega/index.php?s=api/Trans',
+    download: '/omega/index.php?s=api/Download'
 };
+
+window.AppHeader = {
+    'Content-Type': 'application/json;charset=UTF-8',
+    logid: (new Date()).getTime(),
+}
